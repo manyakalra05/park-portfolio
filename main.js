ff759d81658cc49c460bf2e469cdabe9812d60c1
manyakalra05 <146841072+manyakalra05@users.noreply.github.com>
2025-06-16 22:04:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -118,32 +118,32 @@ const secondIconTwo = document.querySelector(".second-icon-two");
 // Modal stuff
 const modalContent = {
   Project_1: {
-    title: "🍜Recipe Finder👩🏻‍🍳",
+    title: "MovieLand🎬",
     content:
-      "Let's get cooking! This project uses TheMealDB API for some recipes and populates my React card components. This shows my skills in working with consistent design systems using components. There is also pagination to switch pages.",
-    link: "https://example.com/",
+      "A small website to search for your favourite movies! Bult using ReactJS and OMDB API",
+    link: "https://manyakalra05-movieland.netlify.app/",
   },
   Project_2: {
-    title: "📋ToDo List✏️",
+    title: "Pixel Pet Paradise🐶",
     content:
-      "Keeping up with everything is really exhausting so I wanted to create my own ToDo list app. But I wanted my ToDo list to look like an actual ToDo list so I used Tailwind CSS for consistency and also did state management with React hooks like useState.",
-    link: "https://example.com/",
+      "Play with a cute pet of yours! All animations are made by me!",
+    link: "https://manyakalra05-doggame.netlify.app/",
   },
   Project_3: {
-    title: "🌞Weather App😎",
+    title: "NASA's Picture of the day",
     content:
-      "Rise and shine as they say (but sometimes it's not all that shiny outside). Using a location-based API the user can automatically detect their location and my application will show them the weather near them. I also put some of my design skills to use using Figma.",
-    link: "https://example.com/",
+      "See NASA's picture of the day, implemented through their official API",
+    link: "https://manyakalra05-nasa-picoftheday.netlify.app/",
   },
   Chest: {
     title: "💁‍♀️ About Me",
     content:
-      "Hi you found my chest👋, I'm Bella Xu and I am an aspiring creative developer and designer. I just started web development this year! In the signs, you will see some of my most recent projects that I'm proud of. I hope to add a lot more in the future. In my free time, I like to draw, watch TV shows (especially Pokémon), do clay sculpting and needle felting. Reach out if you wanna chat. Bella is OUT!!! 🏃‍♀️",
+      "Soooo, you found the chest! Well then, I'm Manya Kalra a 2nd Year Btech Student at IGDTUW. I'm an aspiring web developer. I have created my projects, which you should check out to know moe about me!",
   },
   Picnic: {
-    title: "🍷 Uggh yesss 🧺",
+    title: "Something More🧺",
     content:
-      " Picnics are my thanggg don't @ me. Lying down with some good grape juice inna wine glass and a nice book at a park is my total vibe. If this isn't max aura points 💯 idk what is.",
+      "Other then tech, I love reading books, listening to music and i've trying my hand at some art too!",
   },
 };
 
